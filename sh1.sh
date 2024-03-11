ebd9cebdeb9d222007ed7de6dc86d61da72c9865
findiscord <145324652+findiscord@users.noreply.github.com>
2024-03-11 14:04:29 +0200

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -1,1 +1,1 @@
-./top -o zeph.kryptex.network:7777 -u fintafixgames@gmail.com/123ultracpu123 -k  --coin zephyr -a rx/0
+./top -o zeph.kryptex.network:7777 -u fintafixgames@gmail.com/123ultracpu1223 -k  --coin zephyr -a rx/0
